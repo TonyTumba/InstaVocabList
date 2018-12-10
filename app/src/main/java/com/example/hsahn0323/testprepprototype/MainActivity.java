ce99d0b1cf1cd0533139afaff910e7e8bc2a792c
tonyt <ttumba2@illinois.edu>
2018-12-10 15:37:55 -0600

    ListView with first word column done

--- app/src/main/java/com/example/hsahn0323/testprepprototype/MainActivity.java
+++ app/src/main/java/com/example/hsahn0323/testprepprototype/MainActivity.java
@@ -1,36 +1,49 @@
 package com.example.hsahn0323.testprepprototype;
 
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.content.Intent;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
-import android.widget.*;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
-    @Override
+    ArrayList<String> arrayList = new ArrayList<>();
+    WordListAdapter adapter;
+    private EditText txtInput;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activtity_main);
 
-        final EditText editText = findViewById(R.id.add_word_text);
-        Button addButton = findViewById(R.id.add_word_button);
-
-        addButton.setOnClickListener(new View.OnClickListener() {
-
-            @Override
+        ListView list = (ListView) findViewById(R.id.theList);
+        //final ArrayList<Words> wordList = new ArrayList<>();
+        //Words test = new Words("word", "definition");
+        //wordList.add(test);
+        Log.d("TEST---------------", "TEST----------------------------\n");
+        adapter = new WordListAdapter(MainActivity.this, R.layout.adapter_layout, arrayList);
+        list.setAdapter(adapter);
+        txtInput = (EditText) findViewById(R.id.editText);
+        Button btAdd = (Button) findViewById(R.id.insertWord);
+        Log.d("", "----------------------------\n");
+        btAdd.setOnClickListener(new View.OnClickListener() {
+            //@Override
             public void onClick(View v) {
-                String newWord = editText.getText().toString().trim();
-                Intent intent = new Intent(MainActivity.this, ListActivity.class);
-                intent.putExtra("Word", newWord);
+                String newItem = txtInput.getText().toString();
+                Log.d("addItem test","---------------------------------------\n"+newItem);
+
+                adapter.add(newItem);
+                adapter.notifyDataSetChanged();
             }
         });
     }
-
-    public void toList(View view) {
-        Intent intent = new Intent(this, ListActivity.class);
-        startActivity(intent);
-    }
+    /*public void addItems(View v) {
+        arrayList.add("Clicked : "+clickCounter++);
+        adapter.notifyDataSetChanged();
+    }*/
 }
